c55ef68b8bbdbbe6a8ab47204018727478d8339c
Jeetpal <singhjeetpal99@gmail.com>
2022-08-21 22:06:45 -0500

    Changed logo

--- components/AvatarLogo.js
+++ components/AvatarLogo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Avatar, IconButton } from "@mui/material";
 
 function AvatarLogo({ url, className }) {
-  return <img src={url}  className={`h-10 rounded-full animate-bounce cursor-pointer transition duration-150 transform hover:scale-110 ${className}`} />;
+  return <img src={"https://jooglewebsearch.vercel.app/_next/image?url=%2Fstatic%2Fimg%2FjoogleLogo.png&w=384&q=75"}  className={`h-10 rounded-full animate-bounce cursor-pointer transition duration-150 transform hover:scale-110 ${className}`} />;
 }
 
 export default AvatarLogo;
